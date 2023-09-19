f2575915c4e3495018c7e066dda3a78519247088
AnaRodrigs <mariaanarodrigues2005@gmail.com>
2023-09-19 16:21:57 -0300

    new validators request

--- src/app/student-form/student-form.component.ts
+++ src/app/student-form/student-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Validators } from '@angular/forms';
-import { FormBuilder, FormGroup,  } from '@angular/forms';
+import { FormBuilder, FormGroup, } from '@angular/forms';
 import { Student } from '../student';
 import { StudentService } from '../student.service';
 
@@ -10,45 +10,45 @@ import { StudentService } from '../student.service';
   styleUrls: ['./student-form.component.css']
 })
 
-export class StudentFormComponent implements OnChanges{
-@Input()
-student : Student = {} as  Student;
+export class StudentFormComponent implements OnChanges {
+  @Input()
+  student: Student = {} as Student;
 
-@Output()
-saveEvent = new EventEmitter<Student>();
+  @Output()
+  saveEvent = new EventEmitter<Student>();
 
-@Output()
-CleanEvent = new EventEmitter<void>();
+  @Output()
+  CleanEvent = new EventEmitter<void>();
 
 
-submitted: boolean = false;
-formGroupStudent: FormGroup;
+  submitted: boolean = false;
+  formGroupStudent: FormGroup;
 
   constructor(private studentService: StudentService,
     private formBuilder: FormBuilder) {
     this.formGroupStudent = formBuilder.group({
       id: [''],
-      name: ['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
-      email: ['',[Validators.required, Validators.email]],
-      address: [''],
-      phone: [''],
-      course :[''],
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['',[Validators.required]],
+      phone: ['',[Validators.required]],
+      course: ['',[Validators.required]],
     });
-  } 
+  }
 
-  ngOnChanges(changes: SimpleChanges): void{
-    if(this.student.id){
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.student.id) {
       this.formGroupStudent.setValue(this.student);
     }
-    
+
   }
-  save (){
+  save() {
     this.submitted = true;
-    if (this.formGroupStudent.valid){
-           this.saveEvent.emit(this.formGroupStudent.value);
-            this.formGroupStudent.reset();
-            this.submitted = false;
-        }
+    if (this.formGroupStudent.valid) {
+      this.saveEvent.emit(this.formGroupStudent.value);
+      this.formGroupStudent.reset();
+      this.submitted = false;
+    }
   }
   clean() {
     this.CleanEvent.emit();
@@ -56,10 +56,19 @@ formGroupStudent: FormGroup;
     this.submitted = false;
 
   }
-  get name(): any{
+  get name(): any {
     return this.formGroupStudent.get('name');
   }
-  get email(): any{
+  get email(): any {
     return this.formGroupStudent.get('email');
+  }
+  get course(): any {
+    return this.formGroupStudent.get('course');
+  }
+  get phone(): any {
+    return this.formGroupStudent.get('phone');
+  }
+  get address(): any {
+    return this.formGroupStudent.get('address');
   }
 }
